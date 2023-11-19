3daa85c06cb76ecb76d9ea1d415c2d8f4d602911
Hikmat Rajabli <iamrajabli@outlook.com>
2023-11-20 03:52:46 +0400

    [meta data]

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,20 @@
-import {PropsWithChildren} from "react";
+import { PropsWithChildren } from 'react';
 
 import '@/app/ui/global.css';
-import clsx from "clsx";
-import {inter} from "@/app/ui/fonts";
+import clsx from 'clsx';
+import { inter } from '@/app/ui/fonts';
 
-export default function RootLayout({
-  children,
-}: PropsWithChildren) {
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className={clsx(inter.className, 'antialiased')}>{children}</body>
